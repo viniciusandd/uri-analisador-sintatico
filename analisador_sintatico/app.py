6d206fad6360fe25816eb1c010a845c3cad05840
Vinicius Emanoel Andrade <viniciusemanoelandrade@hotmail.com>
2020-05-27 01:32:08 -0300

    adicionando a blueprint responsável por alocar o recurso principal do sistema

--- analisador_sintatico/app.py
+++ analisador_sintatico/app.py
@@ -2,9 +2,11 @@ from flask import Flask
 
 from analisador_sintatico.ext import configuracao
 from analisador_sintatico.ext import cors
+from analisador_sintatico.blueprints import web
 
 def create_app():
     app = Flask(__name__)
     configuracao.init_app(app)
     cors.init_app(app)
+    web.init_app(app)
     return app
