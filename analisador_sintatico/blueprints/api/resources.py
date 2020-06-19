49fbf40f6091e0e8ab16dfbaefce33daef970765
Vinicius Emanoel Andrade <viniciusemanoelandrade@hotmail.com>
2020-06-19 02:42:23 -0300

    a rota do analisador sintático está realizando toda a criação da tabela, serializando-a e respondendo ao client

--- analisador_sintatico/blueprints/api/resources.py
+++ analisador_sintatico/blueprints/api/resources.py
@@ -1,6 +1,7 @@
 from flask import jsonify
 from flask_restful import Resource
 from .parsers import retornar_parser
+from ..commons import TabelaDeParsing, Pilha, Entrada, Automato
 
 
 class AnalisadorSintatico(Resource):
@@ -9,3 +10,21 @@ class AnalisadorSintatico(Resource):
         args = parser.parse_args()
         sentenca = args['sentenca']        
         
+        pilha = Pilha()
+        entrada = Entrada(sentenca)
+        tabela_parsing = TabelaDeParsing()
+        automato = Automato()
+        
+        iteracoes = 0
+        acao = None
+        while not acao == 'ACEITA':
+            acao = tabela_parsing.acao(pilha.topo(), entrada.inicio())
+            if acao == "DESEMPILHAR_E_LER":
+                pilha.desempilhar()
+                entrada.ler()
+            elif acao and acao != "ACEITA":
+                pilha.modificar_topo(acao)                
+            iteracoes = iteracoes + 1
+            automato.adicionar_linha(pilha, entrada, acao)
+            
+        return jsonify(automato.tabela)
--- analisador_sintatico/blueprints/commons.py
+++ analisador_sintatico/blueprints/commons.py
@@ -1,40 +1,83 @@
+from flask import session
+
+class Auxiliar:
+    def conteudo_to_string(self):
+        return ''.join(self.conteudo)      
+
 class TabelaDeParsing:
     def __init__(self):
         """
         Os valores da tabela são estáticos.
         """
         self.conteudo = {}
-        self.conteudo["S","a"] = "aA"
+        self.conteudo["S","a"] = "aAb"
         self.conteudo["S","b"] = None
         self.conteudo["S","c"] = None
-        self.conteudo["S","$"] = None
+        self.conteudo["S","d"] = None
+        self.conteudo["S","$"] = None     
+
         self.conteudo["A","a"] = "aBc"
         self.conteudo["A","b"] = None
         self.conteudo["A","c"] = None
-        self.conteudo["A","$"] = "&"        
-        self.conteudo["B","a"] = None
-        self.conteudo["B","b"] = None
-        self.conteudo["B","c"] = "cCa"
-        self.conteudo["B","$"] = None        
-        self.conteudo["C","a"] = None
-        self.conteudo["C","b"] = "bB"
-        self.conteudo["C","c"] = "c"
-        self.conteudo["C","$"] = None
-    
-    def buscar(self, nao_terminal = None, terminal = None):
-        return self.conteudo[nao_terminal, terminal] if not nao_terminal and not terminal else self.conteudo
+        self.conteudo["A","d"] = "dC"
+        self.conteudo["A","$"] = None
 
-class Pilha:
+        self.conteudo["B","a"] = None
+        self.conteudo["B","b"] = "bC"
+        self.conteudo["B","c"] = "&"
+        self.conteudo["B","d"] = None
+        self.conteudo["B","$"] = None     
+
+        self.conteudo["C","a"] = None
+        self.conteudo["C","b"] = None
+        self.conteudo["C","c"] = "c"
+        self.conteudo["C","d"] = "dA"
+        self.conteudo["C","$"] = None                     
+                
+    def acao(self, char1, char2):
+        if char1 == char2:
+            if char1 == "$":
+                return "ACEITA"
+            return "DESEMPILHAR_E_LER"
+        return self.conteudo[char1, char2]
+
+class Pilha(Auxiliar):
     def __init__(self):
         self.conteudo = ["$", "S"]
 
-    def buscar(self):
-        return self.conteudo
+    def topo(self):
+        return self.conteudo[len(self.conteudo) - 1]
+    
+    def modificar_topo(self, acao):
+        self.conteudo.pop()
+        if acao == "&":
+            return
+        lista_de_chars = list(acao)
+        lista_de_chars.reverse()        
+        for c in lista_de_chars:
+            self.conteudo.append(c)
+        
+    def desempilhar(self):
+        self.conteudo.pop()    
 
-class Entrada:
+class Entrada(Auxiliar):
     def __init__(self, sentenca):
         self.conteudo = list(sentenca + "$")
     
-    def buscar(self):
-        return self.conteudo    
+    def inicio(self):
+        return self.conteudo[0]
+    
+    def ler(self):
+        self.conteudo.pop(0)
 
+class Automato:
+    def __init__(self):
+        self.tabela = []
+
+    def adicionar_linha(self, pilha: Pilha, entrada: Entrada, acao: str):
+        linha = {
+            "pilha": pilha.conteudo_to_string(),
+            "entrada": entrada.conteudo_to_string(),
+            "acao": acao
+        }
+        self.tabela.append(linha)
