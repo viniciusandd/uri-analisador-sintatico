55ee59062ba2e8b42abba4035d070fb490037e4d
Vinicius Emanoel Andrade <viniciusemanoelandrade@hotmail.com>
2020-06-20 18:04:19 -0300

    habilitando e desabilitando elementos para melhorar a usabilidade do usuário

--- analisador_sintatico/blueprints/web/static/script.js
+++ analisador_sintatico/blueprints/web/static/script.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
     const dataTable = iniciarDataTable();        
     botaoExecucaoCompleta(dataTable);
     botaoExecucaoPorLinha(dataTable);
+    botaoPararReiniciar(dataTable);
 });
 
 const iniciarDataTable = function() {
@@ -24,9 +25,12 @@ function buscarAutomato() {
  }
 
 const botaoExecucaoCompleta = function(dataTable) {
-    $('#btn-execucao-completa').click(function (e) { 
+    $('#btn-execucao-completa').click(function (e) {
         e.preventDefault();
-        dataTable.clear();        
+        dataTable.clear();
+        $(this).prop("disabled", true);
+        $('#btn-execucao-linha').prop("disabled", true);
+        $('#sentenca').prop("disabled", true);
         buscarAutomato().done(function(automato) { 
             dataTable.rows.add(automato).draw(); 
         });
@@ -39,17 +43,32 @@ const botaoExecucaoPorLinha = function(dataTable) {
     $('#btn-execucao-linha').click(function (e) {
         e.preventDefault();
         if (globalAutomato.length == 0) {
+            $('#btn-execucao-completa').prop("disabled", true);
+            $('#sentenca').prop("disabled", true);
             buscarAutomato().done(function(automato) {
                 globalAutomato = automato;
                 dataTable.row.add(globalAutomato[0]).draw();
             });
         } else {
-            if (contadorDeClicks < globalAutomato.length) {
+            if (contadorDeClicks < globalAutomato.length)
                 dataTable.row.add(globalAutomato[contadorDeClicks]).draw();
-            } else {
-                alert('terminou');
+            else {
+                bootbox.alert("A pilha já chegou no final de sua execução. Reinicie a operação.");
+                $(this).prop("disabled", true);
             }
         }
         contadorDeClicks++;
     });    
+}
+
+const botaoPararReiniciar = function(dataTable) {
+    $('#btn-parar-reiniciar').click(function (e) {
+        e.preventDefault();
+        dataTable.clear().draw();
+        $('#btn-execucao-completa').prop("disabled", false);
+        $('#btn-execucao-linha').prop("disabled", false);
+        $('#sentenca').prop("disabled", false);
+        contadorDeClicks = 0;
+        globalAutomato = [];
+    })
 }
